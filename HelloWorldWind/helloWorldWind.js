fd2c29e264cc07c6b24621cbcbcca23cea01cb4e
christopherschmid <46402467+christopherschmid@users.noreply.github.com>
2023-12-21 14:40:55 -0400

    Delete HelloWorldWind/helloWorldWind.js

--- HelloWorldWind/helloWorldWind.js
+++
@@ -1,8 +0,0 @@
-var wwd = new WorldWind.WorldWindow("canvasOne");
-
-wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-
-wwd.addLayer(new WorldWind.CompassLayer());
-wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
